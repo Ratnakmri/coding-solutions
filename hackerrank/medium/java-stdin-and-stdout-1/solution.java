ae6e77c97f2a6bd82d50132110d507e5f17a5cf8
Ratna <gvratnakumari99@gmail.com>
2026-08-18 23:41:13 +0530

    ✅ Java Stdin and Stdout I | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ hackerrank/medium/java-stdin-and-stdout-1/solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read three integers
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        sc.close();
+
+        // Print each integer on a new line
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
