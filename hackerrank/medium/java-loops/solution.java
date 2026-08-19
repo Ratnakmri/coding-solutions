d0f1ddfc58983d81f74c5fe2b9fdc74c42dab0af
Ratna <gvratnakumari99@gmail.com>
2026-08-19 23:32:03 +0530

    ✅ java-loops | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- hackerrank/medium/java-loops/solution.java
+++ hackerrank/medium/java-loops/solution.java
@@ -1,22 +1,21 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.*;
 
-
-
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
-        for (int i=1;i<=10;i++){
-            System.out.println(N+" x "+i+" = "+(N*i));
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int j=0;j<t;j++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int sum = a;
+            for (int i = 0; i < n; i++) {
+                sum += (int)(Math.pow(2, i) * b);
+                System.out.print(sum + " ");
+            }
+            System.out.println();
         }
-
-        bufferedReader.close();
+        in.close();
     }
 }
