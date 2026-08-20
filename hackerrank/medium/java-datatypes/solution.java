5ba6b22b71809308051464b87512160bda8705ab
Ratna <gvratnakumari99@gmail.com>
2026-08-20 23:40:11 +0530

    ✅ Java Datatypes | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-20

--- hackerrank/medium/java-datatypes/solution.java
+++ hackerrank/medium/java-datatypes/solution.java
@@ -1,21 +1,45 @@
 import java.util.*;
 import java.io.*;
 
+
+
 class Solution{
-    public static void main(String []argh){
-        Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int j=0;j<t;j++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
-            int sum = a;
-            for (int i = 0; i < n; i++) {
-                sum += (int)(Math.pow(2, i) * b);
-                System.out.print(sum + " ");
+    public static void main(String []argh)
+    {
+
+
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                                if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) {
+                    System.out.println("* byte");
+                }
+                if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) {
+                    System.out.println("* short");
+                }
+                if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) {
+                    System.out.println("* int");
+                }
+                // long always fits if no exception
+                System.out.println("* long");
             }
-            System.out.println();
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
         }
-        in.close();
     }
 }
+
+
+
