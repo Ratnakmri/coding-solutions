261902cd72c17a5e1213a020d39dc7dd09eda74f
Ratna <gvratnakumari99@gmail.com>
2026-08-21 23:53:42 +0530

    ✅ JAVAPROB03B | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-21

--- codechef/medium/JAVAPROB03B/solution.java
+++ codechef/medium/JAVAPROB03B/solution.java
@@ -1,6 +1,5 @@
 class Codechef {
     public static void main(String[] args) {
-	    // Printing a Hello World!! message
-        System.out.println("Hello World!!");   
+        System.out.println("Hello World!!");
     }
 }
