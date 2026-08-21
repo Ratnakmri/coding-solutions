cb068ad0e0cf5bfc62f82ac08621dc2d641c85a4
Ratna <gvratnakumari99@gmail.com>
2026-08-21 23:42:18 +0530

    ✅ VDATES - Rating 938 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/easy/VDATES---Rating-938/solution.py
@@ -0,0 +1,9 @@
+T=int(input())
+for i in range(T):
+    D,L,R=map(int,input().split())
+    if D<L:
+        print("Too Early")
+    elif D>R:
+        print("Too Late")
+    else:
+        print("Take second dose now")
