9bde5c0ab7b591ac25275a915331040bb096866a
Ratna <gvratnakumari99@gmail.com>
2026-08-24 21:54:27 +0530

    ✅ MFRLE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/MFRLE/solution.java
+++ codechef/medium/MFRLE/solution.java
@@ -6,8 +6,23 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc = new Scaner(System.in);
-		string S = sc.next
+		Scanner sc = new Scanner(System.in);
+		string S = sc.next();
+		sc.close();
+		int[] freq = new int[26];
+		for (char c : S.toCharArray()){
+		    freq[c-'a']++;
+		    
+		}
+		int maxFreq = -1;
+		char answer = 'a';
+		for (int i =0 ; i <26 ; i++){
+		    if (freq[i]>maxFreq){
+		        maxFreq= freq[i];
+		        answer = (char)('a'+ i);
+		    }
+		}
+		System.out.println(answer);
 
 	}
 }
