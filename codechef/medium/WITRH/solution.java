0ca9634eeab2a4454f91e82544edba9eff5b9a4b
Ratna <gvratnakumari99@gmail.com>
2026-08-24 21:26:13 +0530

    ✅ WITRH | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/WITRH/solution.java
+++ codechef/medium/WITRH/solution.java
@@ -6,7 +6,7 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc= new Scanner();
+		Scanner sc= new Scanner(System.in);
 		int X = sc.nextInt();
 		int Y = sc.nextInt();
 		int K = sc.nextInt();
