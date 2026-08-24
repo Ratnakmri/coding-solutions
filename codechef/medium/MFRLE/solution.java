cd1d125533a34c3d70ac91d77858dbb305458929
Ratna <gvratnakumari99@gmail.com>
2026-08-24 22:20:12 +0530

    ✅ MFRLE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/MFRLE/solution.java
+++ codechef/medium/MFRLE/solution.java
@@ -2,30 +2,27 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next().toLowerCase();
+		sc.close();
+		int[] freq = new int[26];
+		for (char c : S.toCharArray()){
+		    freq[c-'a']++;
+		    
+		}
+		int maxFreq = -1;
+		char answer = 'a';
+		for (int i =0 ; i <26 ; i++){
+		    if (freq[i]>maxFreq){
+		        maxFreq= freq[i];
+		        answer = (char)('a'+ i);
+		    }
+		}
+		System.out.println(answer);
 
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String S = sc.next().toLowerCase();  
-
-        int[] freq = new int[26];
-        for (char c : S.toCharArray()) {
-            if (c >= 'a' && c <= 'z') {      
-                freq[c - 'a']++;
-            }
-        }
-
-        int maxFreq = -1;
-        char answer = 'a';
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > maxFreq) {
-                maxFreq = freq[i];
-                answer = (char) ('a' + i);
-            }
-        }
-
-        System.out.println(answer);
-        sc.close();
-    }
+	}
 }
