bc06df7d06a356ee194c65f5506019379a46e934
Ratna <gvratnakumari99@gmail.com>
2026-08-31 20:19:11 +0530

    ✅ SCOCN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/SCOCN/solution.java
+++ codechef/medium/SCOCN/solution.java
@@ -2,22 +2,23 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-import java.util.*;
-
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N]
+        for (int i= 0; i<N; i++{
+            arr[i]=Integer.parseInt(st.nextTokenizer(br.readLine());
+            
         }
-        int X = sc.nextInt();
-
+        int x = Integer.parseInt(st.nextTokenizer(br.readLine());
         int count = 0;
-        int index = -1;
-
+        int index= -1;
+        
         for (int i = 0; i < N; i++) {
             if (arr[i] == X) {
                 count++;
@@ -35,5 +36,6 @@ class Main {
         } else {
             System.out.println(index);
         }
-    }
+        
+	}
 }
