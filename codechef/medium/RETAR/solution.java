ce055a6f0783b783a7a44465aa916272fb117b1f
Ratna <gvratnakumari99@gmail.com>
2026-08-31 19:38:52 +0530

    ✅ RETAR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/RETAR/solution.java
+++ codechef/medium/RETAR/solution.java
@@ -7,7 +7,23 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        // Example: reading integers
+        int X = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        long total = (long)X * A + (long)Y * B;
+
+        if (total >= D) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
 	}
 }
 
