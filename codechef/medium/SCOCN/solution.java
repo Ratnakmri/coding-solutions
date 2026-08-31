84351cf08850fecc7ab4923cbfacb388de2e4654
Ratna <gvratnakumari99@gmail.com>
2026-08-31 20:15:42 +0530

    ✅ SCOCN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

--- codechef/medium/SCOCN/solution.java
+++ codechef/medium/SCOCN/solution.java
@@ -10,7 +10,32 @@ class Codechef
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
+        int[ arr = new int[N]]
+        for (int i= 0; i<N; i++{
+            arr[i]=Integer.parseInt(st.nextTokenizer(br.readLine());
+            
+        }
+        int x = Integer.parseInt(st.nextTokenizer(br.readLine());
+        int count = 0;
+        int index= -1;
         
+        for (int i = 0; i < N; i++) {
+            if (arr[i] == X) {
+                count++;
+                if (count == 2) {
+                    index = i;
+                    break;
+                }
+            }
+        }
 
+        if (count == 0) {
+            System.out.println(-1);
+        } else if (count == 1) {
+            System.out.println(-2);
+        } else {
+            System.out.println(index);
+        }
+        
 	}
 }
