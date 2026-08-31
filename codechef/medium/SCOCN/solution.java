b9dbf2ed653ec53c2905c25fdb5b9f197f533eff
Ratna <gvratnakumari99@gmail.com>
2026-08-31 19:42:23 +0530

    ✅ SCOCN | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/SCOCN/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        
+
+	}
+}
