e7bedb3b5918649f6594e758c068b46d7c5d7ce6
Ratna <gvratnakumari99@gmail.com>
2026-09-07 20:17:28 +0530

    ✅ SAEVI | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-07

--- codechef/medium/SAEVI/solution.java
+++ codechef/medium/SAEVI/solution.java
@@ -6,7 +6,20 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] arr = new int[N];
+		for ( int i= 0 ; i<N ; i++){
+		    arr[i] = sc.nextInt();
+		}
+		long sum = 0;
+        for (int i = 0; i < N; i += 2) { 
+            if (arr[i] > 2 * K) {
+                sum += arr[i];
+            }
+        }
 
-	}
+        System.out.println(sum);
+        sc.close();
 }
